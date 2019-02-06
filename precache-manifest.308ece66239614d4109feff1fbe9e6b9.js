173781df65a8ded0ffb645d1aea56306c0f0efa8
Benedict Ng-Wai <benedict.ngwai@gmail.com>
2019-02-06 22:19:47 +0000

    Updates

---
+++ precache-manifest.308ece66239614d4109feff1fbe9e6b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "92700b96a6acba4945f6",
+    "url": "/profile/static/js/runtime~main.92700b96.js"
+  },
+  {
+    "revision": "2eb86fbe254bc2f671ea",
+    "url": "/profile/static/js/main.2eb86fbe.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/profile/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "2eb86fbe254bc2f671ea",
+    "url": "/profile/static/css/main.53c490a1.chunk.css"
+  },
+  {
+    "revision": "e9c9404b26269efde134b241b137b542",
+    "url": "/profile/index.html"
+  }
+];
