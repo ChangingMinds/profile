8a2d52f38f4afffc19e204aefc4134cbfc7adefc
Benedict Ng-Wai <benedict.ngwai@gmail.com>
2019-02-06 23:02:22 +0000

    all background images and positioning added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import htmlLogo from './html.png';
+import reactLogo from './react.png';
+import herokuLogo from './heroku.png';
+import nodejsLogo from './node-js.png';
+import mongodbLogo from './mongodb.png';
+import expressLogo from './express.png';
+import css3Logo from './css3.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
           <img src={htmlLogo} alt='' />
-        </header>
+          <img src={reactLogo} alt='' />
+          <img src={herokuLogo} alt='' />
+          <img src={nodejsLogo} alt='' />
+          <img src={mongodbLogo} alt='' />
+          <img src={expressLogo} alt='' />
+          <img src={css3Logo} alt='' />
       </div>
     );
   }
