0a2746da16144443cf8248707ae190edf0917e71
Benedict Ng-Wai <benedict.ngwai@gmail.com>
2019-02-20 19:30:48 +0000

    changing direction in web app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React, { Component } from 'react';
-import htmlLogo from './html.png';
-import reactLogo from './react.png';
-import herokuLogo from './heroku.png';
-import nodejsLogo from './node-js.png';
-import mongodbLogo from './mongodb.png';
-import expressLogo from './express.png';
-import css3Logo from './css3.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <img src={htmlLogo} alt='' />
-          <img src={reactLogo} alt='' />
-          <img src={herokuLogo} alt='' />
-          <img src={nodejsLogo} alt='' />
-          <img src={mongodbLogo} alt='' />
-          <img src={expressLogo} alt='' />
-          <img src={css3Logo} alt='' />
-            <div className='name'>
-              <h1>Benedict Ng-Wai</h1>
-                <div className='title'>
-                  <p>Web Developer</p>
-                    <div className='headers'>
-                      <p>about projects contact</p>
-                    </div>
-                </div>
-            </div>
+          
       </div>
     );
   }
